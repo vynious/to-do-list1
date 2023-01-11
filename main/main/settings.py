645be3f81f82aab529b37076b822b43a03f63557
vynious <77824606+vynious@users.noreply.github.com>
2023-01-11 14:16:25 +0800

    add jwt

--- main/main/settings.py
+++ main/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'tdl',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'main.urls'
+CORS_URLS_REGEX = r'^/api/.*'
+CORS_ALLOWED_ORIGINS = []
+if DEBUG:
+    CORS_ALLOWED_ORIGINS += [
+        'https://localhost:8111',
+        'http://localhost:8111'
+    ]
 
 TEMPLATES = [
     {
@@ -133,6 +141,18 @@ CORS_ORIGIN_WHITELIST=[
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'localhost',
-    '127.0.0.1'
+    'http://localhost',
+    'http://127.0.0.1'
 ]
+
+# REST_FRAMEWORK ={
+#     'DEFAULT_AUTHENTICATION_CLASSES':[
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.TokenAuthentication',
+#         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES':[
+#         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+#     ],
+#     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination','PAGE_SIZE':10
+# }
